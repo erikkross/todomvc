8e5303114094092d8209d3179c64be3ed6755cfc
Erikk <erikkross@gmail.com>
2015-08-12 21:11:52 -0400

    please work

---
+++ app/models/todo.js
@@ -0,0 +1,32 @@
+import DS from "ember-data";
+
+var Todo = DS.Model.extend({
+    title: DS.attr('string'), 
+    isCompleted: DS.attr('boolean')
+});
+
+//http://discuss.emberjs.com/t/understanding-why-i-need-reopenclass/1480
+
+Todo.reopenClass({
+	FIXTURES: [
+		{
+			id:1, 
+			title: 'Learn Ember.js', 
+			isCompleted: true
+		},
+		{
+			id:2, 
+			title: '...', 
+			isCompleted: false
+		},
+		{
+			id:3, 
+			title: 'Profile!', 
+			isCompleted: false
+		} 
+		
+	]
+}); 
+
+
+export default Todo;
